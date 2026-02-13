b66e65d35574136a44230d0a79334c8249b36232
abiodunsalavu <eeyas41@nottingham.ac.uk>
2026-02-13 12:09:23 +0000

    adding doxyfile for documentation

---
+++ Worksheet5/adder.h
@@ -0,0 +1,29 @@
+#ifndef MATHSLIB_ADDER_H
+	#define MATHSLIB_ADDER_H
+	#if defined(WIN32) | defined (_WIN32)
+		#ifdef maths_STATIC
+			#define MATHSLIB_API
+		#else
+			#ifdef maths_EXPORTS
+				#define MATHSLIB_API __declspec (dllexport)
+			#else
+				#define MATHSLIB_API __declspec (dllimport)
+			#endif
+		#endif
+	#else
+		#define MATHSLIB_API
+	#endif
+/** @file
+* This files contains the declarations of all exported functions in the maths library
+*/
+/** Adding function
+*This function adds two numbers
+* @param a is the first number 
+* @param b is the second number 
+* @return sum of a and b 
+*/
+
+	MATHSLIB_API int add(int a, int b) ;
+#endif 
+
+			

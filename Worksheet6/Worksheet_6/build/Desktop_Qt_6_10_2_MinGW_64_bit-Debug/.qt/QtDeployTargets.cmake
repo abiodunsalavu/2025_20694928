b33938339ea68c308e7d3f51accb907d83de96d9
abiodunsalavu <eeyas41@nottingham.ac.uk>
2026-03-02 11:23:14 +0000

    Worksheet 6 - Exercise 1

---
+++ Worksheet6/Worksheet_6/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Worksheet_6_FILE C:/Users/asala/2025_20694928/Worksheet6/Worksheet_6/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/Worksheet_6.exe)
+set(__QT_DEPLOY_TARGET_Worksheet_6_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Worksheet_6_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
---
+++ Worksheet6/Worksheet_6/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/deploy_Worksheet_6_509bc6caf2.cmake
@@ -0,0 +1,8 @@
+include("C:/Users/asala/2025_20694928/Worksheet6/Worksheet_6/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/Worksheet_6-plugins.cmake" OPTIONAL)
+set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
+
+qt6_deploy_runtime_dependencies(
+    EXECUTABLE "C:/Users/asala/2025_20694928/Worksheet6/Worksheet_6/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/Worksheet_6.exe"
+    GENERATE_QT_CONF
+)
